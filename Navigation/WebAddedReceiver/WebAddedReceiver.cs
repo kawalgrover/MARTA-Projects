b753c90129d870ed755292adabca498786bac431
Kawal Grover <kawalgrover@gmail.com>
2013-04-25 12:26:21 -0400

    Removed Subsite Receiver

--- Navigation/WebAddedReceiver/WebAddedReceiver.cs
+++ Navigation/WebAddedReceiver/WebAddedReceiver.cs
@@ -24,48 +24,50 @@ namespace Navigation.WebAddedReceiver
         /// </summary>
         public override void WebProvisioned(SPWebEventProperties properties)
         {
-            base.WebProvisioned(properties);
+            //base.WebProvisioned(properties);
 
-            SPWeb currentWeb = properties.Web;
-            InitializeWebAppProperties(currentWeb);
+            //SPWeb currentWeb = properties.Web;
+
+            
+            //InitializeWebAppProperties(currentWeb);
                     
-            TaxonomySession taxonomySession = new TaxonomySession(currentWeb.Site, updateCache: true);
+            //TaxonomySession taxonomySession = new TaxonomySession(currentWeb.Site, updateCache: true);
 
-            //Use the first Termstore object to see if Taxonomy Service is offline or missing.
-            if (taxonomySession.TermStores.Count == 0)
-                throw new InvalidOperationException("The Taxonomy Service is offline or missing.");
+            ////Use the first Termstore object to see if Taxonomy Service is offline or missing.
+            //if (taxonomySession.TermStores.Count == 0)
+            //    throw new InvalidOperationException("The Taxonomy Service is offline or missing.");
 
-            string globalTermSetID = "Empty";
-            string localTermSetID = "Empty";
+            //string globalTermSetID = "Empty";
+            //string localTermSetID = "Empty";
 
-            //Get the values from the property bags.
-            globalTermSetID = Convert.ToString(currentWeb.Site.RootWeb.AllProperties[globalTermSetIDPropertyKey]);
-            localTermSetID = Convert.ToString(currentWeb.Site.RootWeb.AllProperties[localTermSetIDPropertyKey]);
+            ////Get the values from the property bags.
+            //globalTermSetID = Convert.ToString(currentWeb.Site.RootWeb.AllProperties[globalTermSetIDPropertyKey]);
+            //localTermSetID = Convert.ToString(currentWeb.Site.RootWeb.AllProperties[localTermSetIDPropertyKey]);
 
-            TermStore termStore = taxonomySession.TermStores[termStoreName];
+            //TermStore termStore = taxonomySession.TermStores[termStoreName];
 
             
 
-            TermSet globalTermSet = termStore.GetTermSet(new Guid(globalTermSetID));
-            TermSet localTermSet = termStore.GetTermSet(new Guid(localTermSetID));
+            //TermSet globalTermSet = termStore.GetTermSet(new Guid(globalTermSetID));
+            //TermSet localTermSet = termStore.GetTermSet(new Guid(localTermSetID));
 
-            Group termSetGroup = termStore.GetSiteCollectionGroup(currentWeb.Site);
+            //Group termSetGroup = termStore.GetSiteCollectionGroup(currentWeb.Site);
 
-            WebNavigationSettings webNavigationSettings = new WebNavigationSettings(currentWeb);
-            webNavigationSettings.GlobalNavigation.Source = StandardNavigationSource.TaxonomyProvider;
-            webNavigationSettings.CurrentNavigation.Source = StandardNavigationSource.TaxonomyProvider;
+            //WebNavigationSettings webNavigationSettings = new WebNavigationSettings(currentWeb);
+            //webNavigationSettings.GlobalNavigation.Source = StandardNavigationSource.TaxonomyProvider;
+            //webNavigationSettings.CurrentNavigation.Source = StandardNavigationSource.TaxonomyProvider;
 
-            webNavigationSettings.GlobalNavigation.TermStoreId = termStore.Id;
-            webNavigationSettings.GlobalNavigation.TermSetId = new Guid(globalTermSetID);
+            //webNavigationSettings.GlobalNavigation.TermStoreId = termStore.Id;
+            //webNavigationSettings.GlobalNavigation.TermSetId = new Guid(globalTermSetID);
 
-            webNavigationSettings.CurrentNavigation.TermStoreId = termStore.Id;
-            webNavigationSettings.CurrentNavigation.TermSetId = new Guid(localTermSetID);
+            //webNavigationSettings.CurrentNavigation.TermStoreId = termStore.Id;
+            //webNavigationSettings.CurrentNavigation.TermSetId = new Guid(localTermSetID);
 
-            webNavigationSettings.AddNewPagesToNavigation = false;
-            webNavigationSettings.CreateFriendlyUrlsForNewPages = true;
+            //webNavigationSettings.AddNewPagesToNavigation = false;
+            //webNavigationSettings.CreateFriendlyUrlsForNewPages = true;
 
-            webNavigationSettings.Update();
-            currentWeb.Update();
+            //webNavigationSettings.Update();
+            //currentWeb.Update();
         }
 
 
